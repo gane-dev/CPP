bfc867748d0af303b0ab346e4273adaa1af0c0e7
gane_dev <bans2008>
2019-07-07 22:06:13 -0400

    few greedy algos

---
+++ HackerRank/build/CMakeFiles/GreedyFlorist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GreedyFlorist.dir/GreedyFlorist.cpp.o"
+  "GreedyFlorist.pdb"
+  "GreedyFlorist"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GreedyFlorist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
